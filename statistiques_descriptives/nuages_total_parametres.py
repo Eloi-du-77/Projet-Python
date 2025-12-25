bc5acb198daf7cdd002802b49a9c6fd79c6913d2
eloi69 <eloi.veyron@ensae.fr>
2025-12-25 20:05:52 +0000

    Rien

--- statistiques_descriptives/nuages_total_parametres.py
+++ statistiques_descriptives/nuages_total_parametres.py
@@ -1,4 +1,3 @@
-# %%
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
@@ -11,9 +10,10 @@ pd.set_option('display.max_rows', None)
 df_tous_pays=pd.read_pickle("../Toutes_les_df_agregees/df_tous_pays.pkl")
 df_top_10=pd.read_pickle("../Toutes_les_df_agregees/df_top_10.pkl")
 df_top_12=pd.read_pickle("../Toutes_les_df_agregees/df_top_12_sans_NaN.pkl")
+
 #FONCTION DONNANT LE NUAGE ENTRE DEUX PARAMETRES ET LE NOMBRE D'OBSERVATION DES NUAGES
 
-def graphique(a, b, df):
+def nuage_points(a, df, b="score_paralympique",nom_axe_x="Variable",nom_axe_y="Score paralympique"):
     # Créer la figure et les axes
     annees=[2012, 2016, 2020, 2024]
     fig, axes = plt.subplots(2, 2, figsize=(14, 10))
@@ -28,19 +28,16 @@ def graphique(a, b, df):
         # Supprimer les valeurs manquantes
         df_annee = df_annee.dropna(subset=[a, b])
         
-        #print(f"Année {annee}: {len(df_annee)} observations")
-        
-        # Créer le scatter plot
+        #Créer le scatter plot
         ax.scatter(df_annee[b], df_annee[a], alpha=0.6, s=50, c='purple', 
                    edgecolors='black', linewidth=0.5)
         
         # Ajouter les labels
-        ax.set_xlabel(f'{b}', fontsize=11, fontweight='bold')
-        ax.set_ylabel(f'{a}', fontsize=11, fontweight='bold')
+        ax.set_xlabel(nom_axe_x, fontsize=11, fontweight='bold')
+        ax.set_ylabel(nom_axe_y, fontsize=11, fontweight='bold')
         ax.set_title(f'Année {int(annee)}', fontsize=13, fontweight='bold')
         ax.grid(True, alpha=0.3, linestyle='--')
     
     plt.tight_layout()
     plt.show()
 
-# %%
